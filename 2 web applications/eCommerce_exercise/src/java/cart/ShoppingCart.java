8207843a8bf4cb9e1837ddcefa1d73bd9fb13ca0
jpipan <janpipan@gmail.com>
2023-10-16 00:28:40 +0200

    implemented shoppingcart and shoppingcartitem classes

--- 2 web applications/eCommerce_exercise/src/java/cart/ShoppingCart.java
+++ 2 web applications/eCommerce_exercise/src/java/cart/ShoppingCart.java
@@ -8,29 +8,49 @@ import java.util.*;
  * @author juanluis
  */
 public class ShoppingCart {
+    
+    private List<ShoppingCartItem> shoppingCartItems = new ArrayList<ShoppingCartItem>();
 
     public synchronized void addItem(Product product){
-
+        boolean containsItem = false;
+        for (ShoppingCartItem item: shoppingCartItems) {
+            if (item.getProduct().getId() == product.getId()){
+                containsItem = true;
+                item.setQuantity(item.getQuantity()+1);
+                break;
+            }
+        }
+        if (!containsItem){
+            this.shoppingCartItems.add(new ShoppingCartItem(product));
+        }
     }
 
     public synchronized void update(Product product, int quantity) {
-
+        this.shoppingCartItems.get(this.shoppingCartItems.indexOf(product)).setQuantity(quantity);
     }
 
-    //public synchronized List<ShoppingCartItem> getItems() {
-        
-    //}
+    public synchronized List<ShoppingCartItem> getItems() {
+        return shoppingCartItems;
+    }
 
     public synchronized int getNumberOfItems() {
-        return 1;
+        int numberOfItems = 0;
+        for (ShoppingCartItem item: shoppingCartItems) {
+            numberOfItems += item.getQuantity();
+        }
+        return numberOfItems;
     }
 
     public synchronized double getTotal() {
-        return 1.1;
+        double total = 0;
+        for (ShoppingCartItem item: shoppingCartItems) {
+            total += item.getTotal();
+        }
+        return total;
     }
 
     public synchronized void clear() {
-        
+        this.shoppingCartItems.clear();
     }
     
 
--- 2 web applications/eCommerce_exercise/src/java/cart/ShoppingCartItem.java
+++ 2 web applications/eCommerce_exercise/src/java/cart/ShoppingCartItem.java
@@ -7,25 +7,28 @@ import entity.Product;
  * @author juanluis
  */
 public class ShoppingCartItem {
+    
+    private Product product;
+    private int quantity;
 
     public ShoppingCartItem(Product product) {
-
+        this.product = product;
     }   
     
-    //public Product getProduct() {
-    
-    //}
+    public Product getProduct() {
+        return product;
+    }
 
-    //public int getQuantity() {
-
-    //}
+    public int getQuantity() {
+        return quantity;
+    }
 
     public void setQuantity(int quantity) {
-
+        this.quantity = quantity;
     }
 
     public double getTotal() {
-        return 2.2;
+        return product.getPrice() * quantity;
     }
 
 }
