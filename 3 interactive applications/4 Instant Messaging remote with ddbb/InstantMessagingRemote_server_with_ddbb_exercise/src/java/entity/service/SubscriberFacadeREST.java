a77f438acc42c63287564bfe5c9c8e0564bc69e6
jpipan <janpipan@gmail.com>
2023-12-04 19:19:43 +0100

    implemented create and delete subscriber

--- 3 interactive applications/4 Instant Messaging remote with ddbb/InstantMessagingRemote_server_with_ddbb_exercise/src/java/entity/service/SubscriberFacadeREST.java
+++ 3 interactive applications/4 Instant Messaging remote with ddbb/InstantMessagingRemote_server_with_ddbb_exercise/src/java/entity/service/SubscriberFacadeREST.java
@@ -43,13 +43,27 @@ public class SubscriberFacadeREST extends AbstractFacade<Subscriber> {
     // first, check out if the topic from which to subscribe is defined at
     // the Topic table, otherwise return the corresponding message:
     
-    // ...
+    String topicName = entity.getTopic().getName();
+    Query q = em.createQuery("SELECT t FROM Topic t WHERE t.name = :name");
+    q.setParameter("name", topicName);
+    List<Topic> topicList = q.getResultList();
+    if (topicList.isEmpty()) {
+        return new Subscription_check(entity.getTopic(),Subscription_check.Result.NO_TOPIC);
+    }
     
+    entity.setTopic(topicList.get(0));
+    q = em.createQuery("SELECT s FROM Subscriber s WHERE s.user = :user AND s.topic = :topic");
+    q.setParameter("user", entity.getUser());
+    q.setParameter("topic", entity.getTopic());
+    if (q.getResultList().isEmpty()){
+        super.create(entity);
+    }
     // return a Subscription_check after saving the user as subscriber of the
     // topic:
     
+    return new Subscription_check(entity.getTopic(),Subscription_check.Result.OKAY);
     // ...
-    throw new RuntimeException("To be completed by the student");
+    //throw new RuntimeException("To be completed by the student");
     
   }
 
@@ -61,14 +75,38 @@ public class SubscriberFacadeREST extends AbstractFacade<Subscriber> {
     // first, check out if the topic from which to unsubscribe is defined at
     // the Topic table, otherwise return the corresponding message:
     
-    // ...
+    String topicName = entity.getTopic().getName();
+    Query q = em.createQuery("SELECT t FROM Topic t WHERE t.name = :name");
+    q.setParameter("name", topicName);
+    List<Topic> topicList = q.getResultList();
+    
+    if (topicList.isEmpty()) {
+        return new Subscription_check(entity.getTopic(),Subscription_check.Result.NO_TOPIC);
+    }
     
     // check out if the user was subscribed to the intended topic, otherwise
     // return the corresponding message. Return the corresponding message
     // after removing the user from been subscribed to that topic:
+    entity.setTopic(topicList.get(0));
+    q = em.createQuery("SELECT s FROM Subscriber s WHERE s.user = :user AND s.topic = :topic");
+    q.setParameter("user", entity.getUser());
+    q.setParameter("topic", entity.getTopic());
+    List<Subscriber> subscriberList = q.getResultList();
+    if (q.getResultList().isEmpty()){
+        System.out.println("Sub list empty");
+        return new Subscription_check(entity.getTopic(),Subscription_check.Result.NO_SUBSCRIPTION);
+    }
+    
+    q = em.createQuery("DELETE FROM Subscriber s WHERE s.user = :user AND s.topic = :topic");
+    q.setParameter("user", entity.getUser());
+    q.setParameter("topic", entity.getTopic());
+    q.executeUpdate();
+    
+    return new Subscription_check(entity.getTopic(),Subscription_check.Result.OKAY);
+   
     
     // ...
-    throw new RuntimeException("To be completed by the student");
+    //throw new RuntimeException("To be completed by the student");
     
   }
 
